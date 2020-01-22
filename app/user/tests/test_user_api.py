ba23f41bf7024ca9c1e4f8dc39dadc51e1f9d0b1
dkh215 <danielkhunter@gmail.com>
2020-01-22 12:05:46 -0500

    Update tests

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,3 +1,4 @@
+from unittest import skip
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -101,41 +102,44 @@ class PublicUserAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-# class PrivateUserAPITests(TestCase):
-#     """Test API requests that require authentication"""
-#
-#     def setup(self):
-#         self.user = create_user(
-#             email='test@gmail.com',
-#             password='testpass123',
-#             name='Test Name'
-#         )
-#         self.client = APIClient()
-#         self.client.force_authenticate(user=self.user)
-#
-#     def test_retrieve_profile_success(self):
-#         """Test retrieving profile for logged in user"""
-#         res = self.client.get(ME_URL)
-#
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#         self.assertEqual(res.data, {
-#             'name': self.user.name,
-#             'email': self.user.email
-#         })
-#
-#     def test_post_me_not_allowed(self):
-#         """Test that POST is not allowed on the me url"""
-#         res = self.client.post(ME_URL, {})
-#
-#         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-#
-#     def test_update_user_profile(self):
-#         """Test updating the user profile for authenticated user"""
-#         payload = {'name': 'new name', 'password': 'newpassword123'}
-#
-#         res = self.client.patch(ME_URL, payload)
-#
-#         self.user.refresh_from_db()
-#         self.assertEqual(self.user.name, payload['name'])
-#         self.assertTrue(self.user.check_password(payload['password']))
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
+class PrivateUserAPITests(TestCase):
+    """Test API requests that require authentication"""
+
+    def setup(self):
+        self.user = create_user(
+            email='test@gmail.com',
+            password='testpass123',
+            name='Test Name'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    @skip('WIP')
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    @skip('WIP')
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed on the me url"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    @skip('WIP')
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user"""
+        payload = {'name': 'new name', 'password': 'newpassword123'}
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
